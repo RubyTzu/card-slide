8e5cff88e23474f98a0774d58a79ab16cc511285
ruby.chen <rruby6120tw@gmail.com>
2025-07-30 14:05:46 +0800

    feat:modified cards scroll

--- app/component/Cards.tsx
+++ app/component/Cards.tsx
@@ -1,22 +1,47 @@
 "use client";
 
 import { cards } from "@/app/data/cards";
+import { Fragment } from "react";
 
 export const Cards = () => {
-
   return (
-    <section className="relative w-full py-10 bg-stone-200 overflow-hidden scrollbar-hide">
-      <h2 className="text-2xl md:text-3xl font-bold text-center mb-8 text-stone-800">探索這個城市</h2>
-      <div className="snap-x">
-        {/* <div className=""> */}
-          {cards.map((item, i) => (
-            <div key={i} className="bg-white rounded-lg shadow-md p-6 m-4 w-[300px]">
-              <h3 className="text-lg font-bold mb-2">{item.title}</h3>
-              <p className="text-sm text-gray-600">{item.description}</p>
-            </div>
-          ))}
-        {/* </div> */}
+    <section className="relative py-10 bg-stone-200 overflow-hidden scrollbar-hide ">
+      <h2 className="text-2xl md:text-3xl font-bold text-center mb-8 text-stone-800 w-full">探索這個城市</h2>
+      <div className="overflow-x-scroll flex snap-x snap-mandatory w-[100%] mx-auto">
+        {cards.map((item, i) => {
+          return (
+            <Fragment key={i}>
+              {Number(i) === 0 ? (
+                <div className="pl-64">
+                  <Card item={item} />
+                </div>
+              ) : Number(i) !== cards.length - 1 ? (
+                <div className="">
+                  <Card item={item} />
+                </div>
+              ) : (
+                <div className="pr-64">
+                  <Card item={item} />
+                </div>
+              )}
+            </Fragment>
+          );
+        })}
       </div>
     </section>
+  );
+};
+
+type CardItem = {
+  title: string;
+  description: string;
+};
+
+const Card = ({ item }: { item: CardItem }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6 m-4 min-w-[50%] shrink-0 min-h-[300px] flex flex-col justify-center items-center snap-center ">
+      <h3 className="text-lg font-bold mb-2">{item.title}</h3>
+      <p className="text-sm text-gray-600">{item.description}</p>
+    </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { Cards } from "./component/Cards";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start min-h-screen">
-        Hello
+    <div className="">
+      <main className="flex flex-col items-center justify-center min-h-screen w-full">
         <Cards />
       </main>
     </div>
